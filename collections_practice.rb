ace61bb8f5f38dbf531fb9136de3821a5ae77abd
milespucarelli <miles.pucarelli@gmail.com>
2018-12-18 10:13:46 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
 # your code goes here
+def begins_with_r (array)
+  arr = array
+  array = array.select {|element| element.downcase.start_with?("r")}
+  array == arr
+end
+
+def contain_a(array)
+  array.select {|element| element.include?("a")}
+end
+
+def first_wa(array)
+  i = array.index{|element| element.to_s.downcase.start_with?("wa")}
+  array[i]
+end
+
+def remove_non_strings(array)
+  array.delete_if {|element| !element.is_a? String}
+end
+
+def count_elements(array)
+  out = []
+  array.each do |element|
+    count = array.count(element)
+    element[:count] = count
+    out << element
+    array.delete(element)
+  end
+  out
+end
+
+def merge_data(keys, data)
+  keys.map do |key|
+    key.merge(data[0][key[:first_name]])
+  end
+end
+
+def find_cool(cool)
+  cool.select {|element| element[:temperature] == "cool"}
+end
+
+def organize_schools(schools)
+  new_hash = {}
+  schools.each do |schools, locations|
+    locations.each do |location, city|
+      new_hash[city] = []
+    end
+  end
+  schools.each do |schools, locations|
+    locations.each do |location, city|
+      new_hash[city] << schools
+    end
+  end
+  new_hash
+end
